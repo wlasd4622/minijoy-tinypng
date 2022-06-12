33c51be7686094f3c51b0c48deffd0d0cb15a96b
王文龙 <wangwenlong@123.com>
2022-06-12 09:48:35 +0800

    upgrade version

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const commander = require('commander');
+const commander = require("commander");
 const {
   init,
   addKey,
@@ -8,22 +8,18 @@ const {
   emptyKey,
   list,
   watch
-} = require('../lib/subCommand.js');
-const {
-  getKeys
-} = require('../lib/util.js');
-const {
-  compress
-} = require('../lib/handler.js');
-const tinify = require('tinify');
+} = require("../lib/subCommand.js");
+const { getKeys } = require("../lib/util.js");
+const { compress } = require("../lib/handler.js");
+const tinify = require("tinify");
 
 // 主命令
-commander.version(require('../package').version, '-v, --version').usage('[options]').option('-p, --path <newPath>', '压缩后的图片存放到指定路径（使用相对路径）').option('-a, --add <key>', '添加api-key').option('--delete <key>', '删除指定api-key').option('-l, --list', '显示已储存的api-key').option('-w, --watch', '监控当前目录文件变化').option('--empty', '清空已储存的api-key');
+commander.version(require("../package").version, "-v, --version").usage("[options]").option("-p, --path <newPath>", "压缩后的图片存放到指定路径（使用相对路径）").option("-a, --add <key>", "添加api-key").option("--delete <key>", "删除指定api-key").option("-l, --list", "显示已储存的api-key").option("-w, --watch", "监控当前目录文件变化").option("--empty", "清空已储存的api-key");
 
 // 子命令
-commander.command('deep').description('把该目录内的所有图片（含子目录）的图片都进行压缩').action(() => {
+commander.command("deep").description("把该目录内的所有图片（含子目录）的图片都进行压缩").action(() => {
   // deepCompress();
-  console.log('尚未完成，敬请期待.');
+  console.log("尚未完成，敬请期待.");
 });
 
 commander.parse(process.argv);
@@ -49,16 +45,16 @@ if (commander.path) {
   emptyKey();
 } else {
   // 主命令
-  if (typeof commander.args[0] === 'object') {
+  if (typeof commander.args[0] === "object") {
     // 子命令
     // return;
   }
   if (commander.args.length !== 0) {
-    console.log('未知命令');
+    console.log("unknown command，please try minijoy-tinypng -h");
     // return;
   }
   if (getKeys().length === 0) {
-    console.log('请初始化你的api-key');
+    console.log("Please initialize your api-key");
     init();
   } else {
     compress();
--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
-var eslint = require('gulp-eslint');
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var sourcemaps = require("gulp-sourcemaps");
+var del = require("del");
+var eslint = require("gulp-eslint");
 
-gulp.task('eslint', function () {
-    return gulp.src(['./src/**/*.js', './conf/*.js'])
+gulp.task("eslint", function () {
+  return gulp
+    .src(["./src/**/*.js", "./conf/*.js"])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('mocha', function () {});
+gulp.task("mocha", function () {});
 
-gulp.task('clean', ['eslint'], function () {
-    return del([
-        './dist/**/*'
-    ]);
+gulp.task("clean", ["eslint"], function () {
+  return del(["./dist/**/*"]);
 });
 
-gulp.task('development_build', ['clean'], function () {
-    return gulp.src('./src/**/*.js')
+gulp.task("development_build", ["clean"], function () {
+  return gulp
+    .src("./src/**/*.js")
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../src'}))
-    .pipe(gulp.dest('./dist'));
+    .pipe(
+      sourcemaps.write(".", { includeContent: false, sourceRoot: "../src" })
+    )
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('production_build', ['clean'], function () {
-    return gulp.src('./src/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('./dist'));
+gulp.task("production_build", ["clean"], function () {
+  return gulp.src("./src/**/*.js").pipe(babel()).pipe(gulp.dest("./dist"));
 });
 
-gulp.task('debug', ['development_build'], function () {
-    require('./dist/index');
+gulp.task("debug", ["development_build"], function () {
+  require("./dist/index");
 });
 
-gulp.task('default', ['production_build']);
+gulp.task("default", ["production_build"]);
 
-gulp.task('build', ['development_build']);
+gulp.task("build", ["development_build"]);
